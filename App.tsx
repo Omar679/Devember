50e7445bd009d564007ca8a904384f36cc817355
Umar <omarsabiu679@gmail.com>
2024-03-12 18:29:26 +0100

    Commit1

--- App.tsx
+++ App.tsx
@@ -1,11 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+
+const days = [1, 2, 3];
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <FlatList
+        data={days}
+        renderItem={({ item, index }) => (
+          <View key={index} style={styles.box}>
+            <Text style={styles.text}> {item}</Text>
+          </View>
+        )}
+        showsVerticalScrollIndicator={false}
+      />
     </View>
   );
 }
@@ -13,8 +23,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 10,
+  },
+  box: {
+    backgroundColor: "#f9ede3",
+    width: 300,
+    height: 300,
+    borderWidth: 0.5,
+    borderBlockColor: "#9b4521",
+    borderRadius: 20,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "#9b4521",
+    fontSize: 70,
   },
 });
